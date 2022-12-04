6911aacc0cbf2827aa1b1c884539c96f71a350cd
Vitaly Remnev <remnevvs@mail.ru>
2022-12-04 15:47:02 +0300

    Added ordering

--- school/views.py
+++ school/views.py
@@ -6,14 +6,10 @@ from .models import Student
 
 def students_list(request):
     template = 'school/students_list.html'
-    list_students = Student.objects.all().prefetch_related('teachers')
+    list_students = Student.objects.all().prefetch_related('teachers').order_by('group')
 
     context = {
         'object_list': list_students
     }
-
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = 'group'
 
     return render(request, template, context)
